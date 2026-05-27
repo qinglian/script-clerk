992d102a4a3d27f5aea34829f5ec88b2621aa149
清炼 <68185088+qinglian@users.noreply.github.com>
2026-05-27 15:18:31 +0800

    Fix TypeScript errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
 import './assets/main.css'
@@ -21,7 +21,7 @@ const app = createApp(App)
 
 // 注册 Element Plus 图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component as any)
 }
 
 app.use(ElementPlus, { locale: zhCn })
