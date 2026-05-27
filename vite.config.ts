ea4bfb704cf16fa7af8c772f919e8641066a5b00
清炼 <68185088+qinglian@users.noreply.github.com>
2026-05-27 14:48:26 +0800

    Fix: Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/script-clerk/',
   server: {
     host: '0.0.0.0',
     port: 5173
